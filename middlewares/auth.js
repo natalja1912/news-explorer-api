7c89c56576fa360b534e0a8ad2f7309031e2a0bc
Natalia Pavlova <natalja1912@yandex.ru>
2020-12-17 20:34:41 +0300

    feat: auth_jwt_secret

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-err');
 const { authorizationNeededError } = require('../utils/constants');
 
-const { JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b' } = process.env;
+const { jwtSecret } = require('../utils/config');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +17,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, (NODE_ENV === 'production' ? JWT_SECRET : jwtSecret));
   } catch (err) {
     return res
       .status(401)
