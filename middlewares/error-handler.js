bd88137b3c049dc33c537996df47de527d50b057
Natalia Pavlova <natalja1912@yandex.ru>
2021-01-10 19:02:44 +0300

    feat: cookies

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,12 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(req);
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
     .send({
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
-        : message,
+        : req,
     });
   next();
 };
